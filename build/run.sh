1eda9cc5c45c255234cd9cca9c818e7f2d32c1f8
smx-smx <smxdev4@gmail.com>
2017-10-28 02:31:20 +0200

    run.sh: copy shaders only if changed, and read game path from GAMEPATH env var

--- build/run.sh
+++ build/run.sh
@@ -11,10 +11,13 @@ elif [ "$1" == "-d" ]; then
 	use_dbg="-d vs"
 fi
 
-gamePath=/l/Program\ Files\ \(x86\)/EA\ Games/Command\ \&\ Conquer\ Generals\ Zero\ Hour/
+if [ -z "$GAMEPATH" ]; then
+	GAMEPATH=/l/Program\ Files\ \(x86\)/EA\ Games/Command\ \&\ Conquer\ Generals\ Zero\ Hour/
+fi
+
 cmake .. -G"MSYS Makefiles" -DCMAKE_BUILD_TYPE=Debug $use_pdb && make -j$(grep processor /proc/cpuinfo | wc -l) &&
 (
-	cp -Rav ../shaders "$gamePath";
+	cp -Rauv ../shaders "$GAMEPATH";
 	rm openSage.RPT 2>/dev/null;
-	$use_gdb ./openSage.exe $use_dbg -r "$gamePath"
+	$use_gdb ./openSage.exe $use_dbg -r "$GAMEPATH"
 )
